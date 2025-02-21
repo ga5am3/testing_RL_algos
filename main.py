f3d48ed7aa8cd04d7586c9d34e5b32d86291df1e
Gabriel Garcia <jgabrielgarciaa@gmail.com>
2025-02-21 15:58:22 -0500

    main function

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from algos.CrossQ import CrossQSAC_Agent, CrossQTD3_Agent
-
+from utils.buffers import SimpleBuffer
 import gymnasium as gym
 import wandb
 
 if "__main__" == __name__:
 
-    env = gym.make("CartPole-v0")
-    agent = CrossQSAC_Agent(env,use_wandb=True, use_tensorboard=True)
-    agent.train()
+    replay_buffer = SimpleBuffer(max_size=1000, batch_size=50, gamma=0.99, n_steps=1, seed=0)
+
+    env = gym.make("Pendulum-v1")
+    agent = CrossQSAC_Agent(env, replay_buffer=replay_buffer, use_wandb=True)
+    batch_size = 50
+    rollout_eps = 10
+    total_steps = 1000
+    save_freq = 500
+    agent.train(batch_size, rollout_eps, total_steps, save_freq)
