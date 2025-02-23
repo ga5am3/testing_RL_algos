6173b3db2cd39227de01e4c12b22024d1ce76b75
bboato <bruno.boato@cern.ch>
2025-02-23 19:24:21 -0300

    Restructured stuff

--- algos/agent_utils.py
+++ algos/agent_utils.py
@@ -18,7 +18,42 @@ class Base_Agent:
                 next_state, reward, terminated, truncated, info = self.env.step(action)
                 self.replay_buffer.add(state, action, reward, next_state, terminated, truncated)
                 state = next_state
-                
+
+    def rollout(self, eval: bool) -> None:
+        """
+        Rollout the agent in the environment
+        """
+        # print("Rollout step ", _)
+        state, _ = self.env.reset(seed=0)  # TODO: make seed a parameter
+        termination = False
+        truncation = False
+
+        total_ep_reward = 0
+        steps = 0
+        while (not termination) and (not truncation):
+            action = self.select_action(state, eval)
+            next_state, reward, termination, truncation, infos = self.env.step(action)
+            # print(reward)
+            # if self.use_wandb:
+            #     wandb.log({
+            #         "rollout/reward_per_step": reward
+            #     })
+            self.replay_buffer.add(
+                state, action, reward, next_state, termination, truncation
+            )
+            state = next_state
+            steps += 1
+            total_ep_reward += reward
+
+        # print(f"Episode finished in {steps} steps with Average Reward = {total_ep_reward:.2f}")
+        # if self.use_wandb:
+        # wandb.log({
+        #     "rollout/episode_steps": steps,
+        #     "rollout/avg_reward": total_ep_reward/steps,
+        #     "rollout/episode_reward": total_ep_reward
+        # })
+        return total_ep_reward, steps
+
     def get_device(self):
         return "cuda" if torch.cuda.is_available() else "cpu"
     
